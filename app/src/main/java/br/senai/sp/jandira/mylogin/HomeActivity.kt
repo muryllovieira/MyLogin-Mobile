e32db0f6197cfdec0285f14491b25b76ed3b1c8a
MURYLLO SIQUEIRA VIEIRA <22282224@dsn127m1242375.cfp127aluno.local>
2023-03-21 16:56:38 -0300

    terceira tela em andamento

--- app/src/main/java/br/senai/sp/jandira/mylogin/HomeActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/mylogin/HomeActivity.kt
@@ -15,9 +15,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.senai.sp.jandira.mylogin.ui.theme.ui.theme.MyLoginTheme
 
 class HomeActivity : ComponentActivity() {
@@ -45,10 +48,15 @@ fun Home() {
                     .fillMaxWidth()
                     .height(210.dp)
             ) {
-                Image(painter = painterResource(id = R.drawable.paris), contentDescription = "")
+                Image(
+                    painter = painterResource(id = R.drawable.paris),
+                    contentDescription = "",
+                    modifier = Modifier.fillMaxWidth(),
+                    contentScale = ContentScale.FillWidth
+                )
                 Column(
                     modifier = Modifier
-                        .fillMaxWidth()
+                        .fillMaxSize()
                         .padding(15.dp),
                     horizontalAlignment = Alignment.End
                 ) {
@@ -57,19 +65,34 @@ fun Home() {
                         contentDescription = "",
                         modifier = Modifier
                             .clip(
-                                RoundedCornerShape(200.dp)
+                                RoundedCornerShape(210.dp)
                             )
                             .height(61.dp)
                     )
                     Text(text = "Susanna Hoffs", color = Color.White)
+                    Spacer(modifier = Modifier.height(51.dp))
                     Column(
                         modifier = Modifier.fillMaxWidth(),
                         verticalArrangement = Arrangement.Bottom
                     ) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.baseline_email_24),
-                            contentDescription = ""
-                        )
+                        Row() {
+                            Icon(
+                                painter = painterResource(id = R.drawable.baseline_location_on_24),
+                                contentDescription = "",
+                                tint = Color.White
+                            )
+                            Text(text = "You're in Paris", color = Color.White)
+                        }
+                        Column(modifier = Modifier.padding(start = 24.dp)) {
+                            Text(
+                                text = "My Trips",
+                                color = Color.White,
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+
+
                     }
                 }
             }
